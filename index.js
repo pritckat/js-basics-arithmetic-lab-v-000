d7f54a8be1fe438b09b04cc453544fa1f91ba003
Katrina Pritchard <pritckat@gmail.com>
2018-10-10 22:59:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ let newID = 1000000000 + oldID;
 let ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.random() * 20;
 let randomInteger = Math.floor(randomNumber);
-let randomUserID = 1;
+let randomUserID = 1000000000 + randomInteger;
